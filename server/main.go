1bd51da2acdb763b4a38a46b430e12f286863a7c
Ringin Restu Pati <ringinrp21@gmail.com>
2023-02-20 18:00:35 +0700

    update db

--- server/main.go
+++ server/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"wayshub/database"
 	"wayshub/pkg/mysql"
 	"wayshub/routes"
 
-	"github.com/gorilla/handlers"
+	"github.com/gorilla/handlers" // import this package ...
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
@@ -19,6 +21,7 @@ func main() {
 		panic("Failed to load env file")
 	}
 
+	// Another code on this below ...
 	// initial DB
 	mysql.DatabaseInit()
 
@@ -28,16 +31,21 @@ func main() {
 	r := mux.NewRouter()
 
 	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
+	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads")))) // add this code
+
+	// fmt.Println("server running localhost:5000")
+	// http.ListenAndServe("localhost:5000", r)
+
 	// Setup allowed Header, Method, and Origin for CORS on this below code ...
 	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
 	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
-	port := "5000"
-	//path file
-	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads")))) // add this code
+	var port = os.Getenv("PORT")
+	fmt.Println("server running online:" + port)
 
-	http.ListenAndServe("localhost:"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
-	// http.ListenAndServe("localhost:5000", r)
-
+	// Embed the setup allowed in 2 parameter on this below code ...
+	http.ListenAndServe(":"+port, handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+	fmt.Println("server running localhost:5000")
+	http.ListenAndServe(":"+port, r)
 }
